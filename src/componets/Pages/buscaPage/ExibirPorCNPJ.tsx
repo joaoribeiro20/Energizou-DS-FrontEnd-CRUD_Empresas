ec481f9c37012147a10ddb242d567d64d1c12ec1
João Vitor Alves Ribeiro <joao.ribeiro@phb.com>
2023-10-31 16:47:24 -0300

    funçoes buscar todas empresas e buscar por cnpj prontas

--- src/componets/Pages/buscaPage/ExibirPorCNPJ.tsx
+++ src/componets/Pages/buscaPage/ExibirPorCNPJ.tsx
@@ -7,29 +7,47 @@ import { MyData } from '../../types.ts'; // Importe o tipo definido
 
 
 const ExibirPorCNPJ: React.FC = () => {
-  const [data, setData] = useState<[MyData] | null>(null);
+  const [data, setData] = useState<MyData| null>(null);
+  const [inputValue, setInputValue] = useState('');
 
-  useEffect(() => {
-    // Faça uma solicitação Axios no useEffect para buscar os dados
-    axios.get<[MyData]>('http://localhost:8088/allSearch/')
-      .then(response => {
-        console.log(response.data)
-        setData(response.data); // Aqui você recebe os dados tipados
 
-      })
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // Atualiza o valor de inputValue com o valor do input quando ele é alterado.
+    setInputValue(event.target.value);
+  };
 
-      .catch(error => {
-        // Lide com erros aqui
-        console.log(error)
-      });
-  }, []);
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    alert('Valor do input: ' + inputValue);
+    // O valor do input é acessado diretamente da variável inputValue.
+   
+      // Faça uma solicitação Axios no useEffect para buscar os dados
+      axios.get<MyData>(`https://jsonplaceholder.typicode.com/todos/${inputValue}`)
+        .then(response => {
+          console.log(response.data)
+          setData(response.data); // Aqui você recebe os dados tipados
+  
+        })
+  
+        .catch(error => {
+          // Lide com erros aqui
+          console.log(error)
+        });
+    }
 
+  
   return (
     <>
       <h3>Filtrar por CNJP</h3>
       <div>
-        <input type="text" name="" id="" />
-        <button>Buscar</button>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+          <button type="submit">Enviar</button>
+        </form>
       </div>
     
      
@@ -37,14 +55,18 @@ const ExibirPorCNPJ: React.FC = () => {
              <div className='containerEmpresas'>
              <div className='containerEmpresa'>
                <ul>
-                 <h1>Empresas</h1>
+               <li>nome: {data.title}</li>
+                 <li>nome da empresa: {data.id}</li>
+                 <li>telefone: {data.userId}</li>
+                 <li>Email: {data.completed}</li>
+                 {/* <h1>Empresas</h1>
                  <li>nome: {data[0].nomeCliente}</li>
                  <li>nome da empresa: {data[0].nomeEmpresa}</li>
                  <li>telefone: {data[0].tefelone}</li>
                  <li>Email: {data[0].email}</li>
                  <li>CEP: {data[0].cep}</li>
                  <li>Endereço: {data[0].endereco}  n{data[0].numero} </li>
-                 <li>CNPJ: {data[0].cnpj}</li>
+                 <li>CNPJ: {data[0].cnpj}</li> */}
                </ul>
              </div>
              </div>
@@ -53,5 +75,6 @@ const ExibirPorCNPJ: React.FC = () => {
     </>
   );
 };
+
 
 export default ExibirPorCNPJ
--- src/componets/Pages/buscaPage/GetExibir.tsx
+++ src/componets/Pages/buscaPage/GetExibir.tsx
@@ -1,54 +1,78 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { MyData } from '../../types.ts'; // Importe o tipo definido
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { MyData } from "../../types";
 
-import Header from '../../Header.tsx';
-import Footer from '../../Footer.tsx';
 import "./StylesBusca.css"
-import ExibirPorCNPJ from './ExibirPorCNPJ.tsx';
+import ExibirPorCNPJ from "./ExibirPorCNPJ";
 
-
-
-const MyComponent: React.FC = () => {
-  const [data, setData] = useState([]);
+const GetExibir:React.FC = () => {
+    const [data, setData] = useState<[MyData] | null>(null);
   
- 
+    useEffect(() => {
+      // Faça uma solicitação Axios no useEffect para buscar os dados
+      axios.get<[MyData]>('https://jsonplaceholder.typicode.com/users')
+        .then(response => {
+          console.log(response.data)
+          setData(response.data); // Aqui você recebe os dados tipados
+          
+        })
+  
+        .catch(error => {
+          // Lide com erros aqui
+          console.log(error)
+        });
+    }, []);
 
-  useEffect(() => {
-    // Faça uma solicitação Axios no useEffect para buscar os dados
-    axios.get(' https://pokeapi.co/api/v2/pokemon')
-      .then(response => {
-        console.log(response.data.results)
-        setData(response.data.results); // Aqui você recebe os dados tipados
 
-        for (const objeto of response.data.results
-          ) {
-          const informacao = objeto.name;
-         
-          console.log(`Informação: ${informacao}`);
-          <div>
-            {informacao}
-          </div>
-        }
-     
-      })
-      .catch(error => {
-        // Lide com erros aqui
-        console.log(error)
-      });
-  }, []);
+    const itensRenderizados = data?.map((data1, index) => (
+        <div className='containerEmpresas' key={index}>
+            <div className='containerEmpresa' >
+          <p>nome da empresa: {data1.title}</p>
+          <p>nome: {data1.userId}</p>
+          <p>telefone: {data1.id}</p>
+          <p>Email: {data1.completed}</p>
+        </div>
+            </div>
+        
+      ));
 
-  return (
-    <>
-      <Header />
-      <div className='containerMainBucar'>
-        <h2>Lista de todas empresas Cadastradas</h2>
-          {}
-         {/* <ExibirPorCNPJ /> */}
-        <Footer />
-      </div>
-    </>
 
-  );
-};
-export default MyComponent;
+
+    return (
+      <>
+      <ExibirPorCNPJ />
+        <h2 className="containerMainBucar">lista de empresas cadastradas</h2>
+      {itensRenderizados}
+
+      
+       
+       {/*  { data?.map((((data1, index) => {
+            {console.log(data1)}
+            <div className='containerEmpresas'>
+               <div key={index} className='containerEmpresa'>
+                     <p>nome da empresa: {data1.title}</p> */}
+             
+               {/*   <ul>
+                 <h1>teste</h1>
+                 <li>nome: {data1.userId}</li>
+                   <li>nome da empresa: {data1.title}</li>
+                   <li>telefone: {data1.id}</li>
+                   <li>Email: {data1.completed}</li> */}
+                   {/* <h1>Empresas</h1>
+                   <li>nome: {data1.nomeCliente}</li>
+                   <li>nome da empresa: {data1.nomeEmpresa}</li>
+                   <li>telefone: {data1.tefelone}</li>
+                   <li>Email: {data1.email}</li>
+                   <li>CEP: {data1.cep}</li>
+                   <li>Endereço: {data1.endereco}  n{data1.numero} </li>
+                   <li>CNPJ: {data1.cnpj}</li> */}
+                 {/* </ul> */}
+             {/*   </div>
+               </div> */}
+              
+       {/*  })))}  */}
+      </>
+    );
+  };
+
+  export default GetExibir
